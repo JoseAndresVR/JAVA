5a8299ba60ae59947de97b401d85aadb9062a243
Jose <jv295222@gmail.com>
2024-03-13 17:10:22 -0500

    Reto 9 H

--- CLASES/src/com/krakedev/TestCalculadora.java
+++ CLASES/src/com/krakedev/TestCalculadora.java
@@ -4,12 +4,17 @@ public class TestCalculadora {
 
 	public static void main(String[] args) {
 		Calculadora x=new Calculadora();
-		int Rsa,Rrt;
-		Rsa = x.sumar(5, 3);
-		Rrt = x.restar(5, 8);
+		int Rs,Rr;
+		Rs = x.suma(5, 3);
+		Rr = x.restar(5, 8);
+		System.out.println(Rs);
+		System.out.println(Rr);
 		
-		System.out.println(Rsa);
-		System.out.println(Rrt);
+		
+		System.out.println("Multiplicacion: "+x.multiplicar(5, 5));
+		System.out.println("Division: "+x.dividir(10, 3));
+		System.out.println("Promedio de 7.50 - 8.30 - 9.90: "+x.promediar(7.50, 8.50, 9.90));
+		x.mostrarResul();
 		
 	}
 
