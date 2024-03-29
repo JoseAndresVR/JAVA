e6763815bea62ead096bc4411b7deb7652986de2
Jose <jv295222@gmail.com>
2024-03-28 21:56:01 -0500

    Metodos Post y put aprendidos

--- RestWebService/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
+++ RestWebService/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -1,19 +1,47 @@
 package com.krakedev.servicios;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.Path;
 
-@Path("/customers")
+import com.krakedev.entidades.Entidades;
+@Path("clientes")
+//@Path("/customers")
 public class ServicioClientes {
-
-    @GET
-    @Path("/m1")
-    @Produces(MediaType.TEXT_PLAIN)
+	@Path("metodo1")
+	@GET
+   // @GET
+   // @Path("/m1")
+   // @Produces(MediaType.TEXT_PLAIN)
     public String saludar() {
         return "¡Hola desde el método saludar!";
     }
+	
+	@Path("buscar")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Entidades cliente() {
+		Entidades cl = new Entidades("0804514875","Jose","1");
+		return cl;
+	}
+	
+	@Path("ins")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertar(Entidades clienteI) {
+		System.out.println("Cliente nuevo>>>>>>>>>>>>>>>>>>>>"+clienteI);
+	}
+	
+	@Path("act")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void actualizar(Entidades clienteI) {
+		System.out.println("Cliente actualizado>>>>>>>>>>>>>>>>>>>>"+clienteI);
+	}
 }
 
 
